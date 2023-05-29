4ae5d9a80a1d58b44d8bd91297b751992be30645
Jakub Kosakowski <jkosakowski602@gmail.com>
2023-05-29 18:28:40 +0200

    Create and add to URLs HomePageView

--- shop_app/views.py
+++ shop_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import request
+from django.views import View
 
-# Create your views here.
+
+class HomePageView(View):
+    def get(self, request):
+        return render(request, 'home.html')
