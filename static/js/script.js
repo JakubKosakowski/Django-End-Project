5d84bce3061b868f1a5571489d28dad9c363ee10
Jakub Kosakowski <jkosakowski602@gmail.com>
2023-06-02 13:38:09 +0200

    Add order button

--- static/js/script.js
+++ static/js/script.js
@@ -4,7 +4,7 @@ document.querySelector('.add').addEventListener('click', function(){
 
 document.querySelector('.sub').addEventListener('click', function(){
     amount = document.querySelector('.amount-number');
-    if(amount.value > 0){
+    if(amount.value > 1){
         amount.value--;
     }
 });
